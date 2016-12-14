630a73bf0c7f9690ccf7a4d5266484b87cb36b4b
Deathmynd <deathmynds@gmail.com>
2016-12-15 00:39:56 +0400

    fix #1

--- binaryTree.hs
+++ binaryTree.hs
@@ -1,4 +1,4 @@
-data BinaryTree = EmptyTree | Node Integer BinaryTree BinaryTree-- deriving (Show, Read, Eq)
+data BinaryTree = EmptyTree | Node Integer BinaryTree BinaryTree -- deriving (Eq)
 singleton :: Integer -> BinaryTree    
 singleton x = Node x EmptyTree EmptyTree    
     
@@ -17,19 +17,19 @@ minValue (Node a left _) = minValue left
 remove :: BinaryTree -> Integer -> BinaryTree
 remove EmptyTree a = emptyTree
 remove (Node a EmptyTree EmptyTree) x | (a == x) = EmptyTree
-									  | otherwise = (Node a EmptyTree EmptyTree)
+    | otherwise = (Node a EmptyTree EmptyTree)
 remove (Node a left EmptyTree) x | (a == x) = left
-								 | (a > x) = Node a (remove left x) EmptyTree
-								 | (a < x) = Node a left EmptyTree
-								 
+    | (a > x) = Node a (remove left x) EmptyTree
+    | (a < x) = Node a left EmptyTree
+
 remove (Node a EmptyTree right) x | (a == x) = right
-								  | (a > x) = Node a EmptyTree right
-								  | (a < x) = Node a EmptyTree (remove right x)
+    | (a > x) = Node a EmptyTree right
+    | (a < x) = Node a EmptyTree (remove right x)
 remove (Node a left right) x | (x < a) = Node a (remove left x) right
-							 | (x > a) = Node a left (remove right x)
-							 | (x == a) = let rightMinimum = minValue right in
-										 Node rightMinimum left (remove right rightMinimum)
-	
+    | (x > a) = Node a left (remove right x)
+    | (x == a) = let rightMinimum = minValue right in
+        Node rightMinimum left (remove right rightMinimum)
+
 emptyTree :: BinaryTree
 emptyTree = EmptyTree
 
@@ -40,32 +40,37 @@ containsElement (Node a left right) x
     | x < a  = containsElement left x  
     | x > a  = containsElement right x 
 
-	
 maxValue :: BinaryTree -> Integer
 maxValue EmptyTree = error "Tree is empty"
 maxValue (Node a _ EmptyTree) = a
 maxValue (Node a _ right) = maxValue right
 
 nearestGE :: BinaryTree -> Integer -> Integer
-nearestGE EmptyTree x = x-1
-nearestGE (Node a left right) x	| (maxValue (Node a left right) < x) = error "value is more than the maximum value in a tree"
-								| (a == x) = a
-								| (x < a) = let newX = nearestGE left x in
-																if (newX < x) then a
-																			  else newX
-								| (x > a) = nearestGE right x
+nearestGE EmptyTree x = error "Tree is empty"
+nearestGE tree x = 
+    if (maxValue tree < x) 
+    then error "value is more than the maximum value in a tree"
+    else nearestGEmain tree x
+
+nearestGEmain :: BinaryTree -> Integer -> Integer
+nearestGEmain EmptyTree x = x-1
+nearestGEmain (Node a left right) x | (a == x) = a
+    | (x < a) = let newX = nearestGEmain left x in
+        if (newX < x) then a
+            else newX
+    | (x > a) = nearestGEmain right x
 
 convertTreeToList :: BinaryTree -> [Integer] -> [Integer]
 convertTreeToList EmptyTree list = list
 convertTreeToList (Node a left right) list = let listWithA = list ++ [a] in
-											 let listWithAWithLeft = convertTreeToList left listWithA in
-											 convertTreeToList right listWithAWithLeft
+    let listWithAWithLeft = convertTreeToList left listWithA in
+    convertTreeToList right listWithAWithLeft
 
 convertListToTree :: [Integer] -> BinaryTree -> BinaryTree
 convertListToTree [] tree = tree
 convertListToTree (h:t) tree = let newTree = insert tree h in
-														convertListToTree t newTree   
-											 
+    convertListToTree t newTree   
+
 listFromTree :: BinaryTree -> [Integer]
 listFromTree tree = convertTreeToList tree []
 
@@ -75,10 +80,10 @@ treeFromList list = convertListToTree list EmptyTree
 checkTree :: String -> String
 checkTree str = 
 
-	let x = emptyTree `insert` 8 `insert` 4 `insert` 2 `insert` 6 `insert` 12 `insert` 10 `insert` 14 in
-	--let x = [2, 4, 3, 5, 7] in
-	--let tree = treeFromList x in
+    let x = emptyTree `insert` 8 `insert` 4 `insert` 2 `insert` 6 `insert` 12 `insert` 10 `insert` 14 in
+    --let x = [2, 4, 3, 5, 7] in
+    --let tree = treeFromList x in
 
-	show $ nearestGE x 15
+    show $ nearestGE x 9
 
 main = interact checkTree
--- fold.hs
+++ fold.hs
@@ -24,40 +24,38 @@ filterNew f list = foldrNew (\h t -> if (f h) then h:t else t) [] list
 
 maxByNew :: (a -> Integer) -> [a] -> a
 maxByNew f (h:t) =
-				foldlNew (\max h -> if (f max) > (f h) then max else h) h t
+    foldlNew (\max h -> if (f max) > (f h) then max else h) h t
 
 minByNew :: (a -> Integer) -> [a] -> a
 minByNew f (h:t) = 
-				foldlNew (\min h -> if (f min) < (f h) then min else h) h t
+    foldlNew (\min h -> if (f min) < (f h) then min else h) h t
 
 reverseNew :: [a] -> [a]
 reverseNew [] = []
-reverseNew list = foldrNew (\h t -> t ++ [h]) [] list
+reverseNew list = foldlNew (\t h -> h:t) [] list
 
 elementAtNew :: Integer -> [a] -> a
 elementAtNew _ [] = error "list is empty"
-elementAtNew x list |	((length list) <= (fromIntegral x)) = error "no element at list"
-					|	otherwise = res
-								where (res, _) =
-										foldlNew (\(res,x) h -> if (x == 0) then (h, x-1) else (res, x-1)) (head list, x) list
+elementAtNew x list | ((length list) <= (fromIntegral x)) = error "no element at list"
+    | otherwise = res
+      where (res, _) = foldlNew (\(res,x) h -> if (x == 0) then (h, x-1) else (res, x-1)) (head list, x) list
 
 indexOfNew :: String -> [String] -> Integer
 indexOfNew _ [] = error "list is empty"
 indexOfNew x list = if (res == -1) then error "element not found" else res
-					where (res, _, _) =
-						foldlNew (\(res, count, x) h -> if (h == x) then (count, count+1, x) else (res, count+1, x)) (-1, 0, x) list
+    where (res, _, _) = foldlNew (\(res, count, x) h -> if (h == x) then (count, count+1, x) else (res, count+1, x)) (-1, 0, x) list
 
 -- main
 test :: String -> String
 test str =
-	--let x = mapNew (\x -> if (x > 1) then True else False) [1,2] in
-	--let x = flatMapNew (\x -> [x, x]) [1, 2] in
-	--let x = concatNew [1, 2] [3, 4] in	
-	--let x = filterNew (\x -> if (x > 1) then True else False) [1,2] in
-	--let x = maxByNew (\x -> -x) [1,2] in	
-	--let x = minByNew (\x -> -x) [1,2] in
-	--let x = reverseNew [1,2,3] in
-	--let x = elementAtNew 3 [1,2,3] in
-	let x = indexOfNew "we" ["qwe", "asd", "zxc"] in
-	show $ x
+    --let x = mapNew (\x -> if (x > 1) then True else False) [1,2] in
+    --let x = flatMapNew (\x -> [x, x]) [1, 2] in
+    --let x = concatNew [1, 2] [3, 4] in	
+    --let x = filterNew (\x -> if (x > 1) then True else False) [1,2] in
+    --let x = maxByNew (\x -> -x) [1,2] in	
+    --let x = minByNew (\x -> -x) [1,2] in
+    --let x = reverseNew [1,2,3] in
+    let x = elementAtNew 2 [1,2,3,4,5] in
+    --let x = indexOfNew "we" ["qwe", "asd", "zxc"] in
+    show $ x
 main = interact test
